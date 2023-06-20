f82c8fa929bfb5dfba8807ee3e8bab8cc4e6b133
frntprog <frntprog@gmail.com>
2023-06-20 19:29:22 +0300

    redux toolkit

---
+++ ReduxToolkit/src/store.ts
@@ -0,0 +1,27 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const initialState = { value: { username: "" } };
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.value = action.payload;
+    },
+
+    logout: (state) => {
+      state.value = initialState.value;
+    },
+  },
+});
+
+console.log(userSlice);
+
+export const { login, logout } = userSlice.actions;
+
+export const store = configureStore({
+  reducer: {
+    user: userSlice.reducer,
+  },
+});
